8b435b8991e6b5595e32c6177d4bfd25c830ce90
jcamilovallejos <j.vallejosg@uniandes.edu.co>
2024-04-27 16:41:18 -0500

    Adicionar el primer test a DataItemAlbumsTests

---
+++ app/src/test/java/com/example/mis4203movilvinilosjpc/DataItemAlbumsTests.kt
@@ -0,0 +1,34 @@
+package com.example.mis4203movilvinilosjpc
+
+import com.example.mis4203movilvinilosjpc.ActivityPrincipal.Albums.Data.Modelo.DataItemAlbums
+import com.example.mis4203movilvinilosjpc.ActivityPrincipal.Albums.Data.Modelo.Track
+import com.example.mis4203movilvinilosjpc.ActivityPrincipal.Albums.Data.Modelo.Performer
+import com.example.mis4203movilvinilosjpc.ActivityPrincipal.Albums.Data.Modelo.Comment
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.Date
+
+
+class DataItemAlbumsTests {
+
+    @Test
+    fun testDataItemAlbums() {
+        val album = DataItemAlbums(
+            id = "1",
+            name = "AlbumName",
+            cover = "CoverURL",
+            releaseDate = "2024-04-26",
+            description = "AlbumDescription",
+            genre = "Rock",
+            recordLabel = "RecordLabel"
+        )
+
+        assertEquals("1", album.id)
+        assertEquals("AlbumName", album.name)
+        assertEquals("CoverURL", album.cover)
+        assertEquals("2024-04-26", album.releaseDate)
+        assertEquals("AlbumDescription", album.description)
+        assertEquals("Rock", album.genre)
+        assertEquals("RecordLabel", album.recordLabel)
+    }
+}
