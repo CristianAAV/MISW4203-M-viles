63800086a96d02ec59ff80209acf7ede4f19e1f4
echaparroa-uniandes <e.chaparroa@uniandes.edu.co>
2024-05-25 16:49:10 -0500

    tests para crear y comentar album

--- app/src/androidTest/test_e2e/specs/albums.comentar.e2e.js
+++ app/src/androidTest/test_e2e/specs/albums.comentar.e2e.js
@@ -34,6 +34,34 @@ describe("Test Comment Album", () => {
         assert.include(commetText, 'Comentarios');
     });
 
+    it('should display the detail album write comment on the screen', async function() {
+        const textcommentElement = await $('//android.view.View[@content-desc="textFieldcomentAlbumsDetail"]');
+        await textcommentElement.click();
+        await browser.pause(1000); // Espera 1 segundo (1000 milisegundos)
+        await browser.keys('Buen Album'); // Simular la entrada de texto
+        await browser.pause(2000); // Espera 2 segundos (2000 milisegundos)
+        await textcommentElement.click();
+        const commetTextValue = await $('//android.widget.EditText[@text="Buen Album"]').getAttribute('text');
+        assert.isNotEmpty(commetTextValue, 'Buen Album');
+        assert.include(commetTextValue, 'Buen Album');
+    });
 
-
+    it('should display the detail album comment album on the screen', async function() {
+        const textcommentElement = await $('//android.view.View[@content-desc="textFieldcomentAlbumsDetail"]');
+        await textcommentElement.click();
+        await browser.pause(1000); // Espera 1 segundo (1000 milisegundos)
+        await browser.keys(', me encanta'); // Simular la entrada de texto
+        await browser.pause(1000); // Espera 2 segundos (2000 milisegundos)
+        await driver.back();
+        await browser.pause(2000); // Espera 2 segundos (2000 milisegundos)
+        const commetTextValue = await $('//android.widget.EditText[@text="Buen Album, me encanta"]').getAttribute('text');
+        await browser.pause(1000); // Espera 2 segundos (2000 milisegundos)
+        assert.isNotEmpty(commetTextValue, 'Buen Album, me encanta');
+        assert.include(commetTextValue, 'Buen Album, me encanta');
+        await $('//android.widget.TextView[@content-desc="textBtnComentAlbumsDetail"]').click();
+        const commentsAlbum = await $$('//android.widget.TextView[@content-desc="commentAlbumsDetail"]');
+        const commentAlbum = await commentsAlbum[1].getAttribute('text'); // Corrección aquí
+        assert.isNotEmpty(commentAlbum, '2. Buen Album, me encanta'); // Cambio aquí
+        assert.include(commentAlbum, '2. Buen Album, me encanta'); // Cambio aquí
+    });
 });
